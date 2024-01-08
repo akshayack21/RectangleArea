ead72153dbcf96d65067ad520870a0f732fa01f8
akshayack21 <155761003+akshayack21@users.noreply.github.com>
2024-01-08 18:28:52 +0530

    Add files via upload

---
+++ RectangleArea.java
@@ -0,0 +1,15 @@
+public class RectangleArea {
+    public static void main(String[] args) {
+        
+        double length = 3; // in centimeters
+        double width = 6; // in centimeters
+        
+       
+        double area = length * width;
+        
+        
+        System.out.println("Length of the rectangle: " + length + " cm");
+        System.out.println("Width of the rectangle: " + width + " cm");
+        System.out.println("Area of the rectangle: " + area + " square cm");
+    }
+}
